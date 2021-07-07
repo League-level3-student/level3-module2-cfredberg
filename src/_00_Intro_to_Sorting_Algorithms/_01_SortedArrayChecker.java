9e1d8dbd75e33d510f852f3e9f73fe758870b7da
cfredberg <53453343+cfredberg@users.noreply.github.com>
2021-07-07 16:57:36 -0700

    *-visualarraysorter IP

--- src/_00_Intro_to_Sorting_Algorithms/_01_SortedArrayChecker.java
+++ src/_00_Intro_to_Sorting_Algorithms/_01_SortedArrayChecker.java
@@ -9,8 +9,15 @@ public class _01_SortedArrayChecker {
      * and false otherwise.
      */
     public static boolean intArraySorted(int[] arr) {
-        
-        return false;
+        for (int i = 0; i < arr.length; i++) {
+        	for (int j = 0; j < arr.length-1; j++) {
+        		if (arr[j] > arr[j+1]) {
+        			return false;
+        		}
+        	}
+        }
+    	
+        return true;
     }
 
     /*
@@ -20,7 +27,18 @@ public class _01_SortedArrayChecker {
      * The method returns true if the double array is in ascending order
      * and false otherwise.
      */
-
+    
+    public static boolean doubleArraySorted(Double[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+        	for (int j = 0; j < arr.length-1; j++) {
+        		if (arr[j] > arr[j+1]) {
+        			return false;
+        		}
+        	}
+        }
+    	
+        return true;
+    }
 
     /*
      * 3. Write a static method called charArraySorted.
@@ -30,7 +48,18 @@ public class _01_SortedArrayChecker {
      * order and false otherwise (You can compare characters just like
      * integers).
      */
-
+    
+    public static boolean charArraySorted(char[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+        	for (int j = 0; j < arr.length-1; j++) {
+        		if (arr[j] > arr[j+1]) {
+        			return false;
+        		}
+        	}
+        }
+    	
+        return true;
+    }
 
     /*
      * 4. Write a static method called stringArraySorted.
@@ -39,5 +68,16 @@ public class _01_SortedArrayChecker {
      *  The method returns true if the String array is in alphabetical
      *  order and false otherwise (Use the compareTo(String) method).
      */
-
+    
+    public static boolean stringArraySorted(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+        	for (int j = 0; j < arr.length-1; j++) {
+        		if (arr[j].compareTo(arr[j+1]) > 0) {
+        			return false;
+        		}
+        	}
+        }
+    	
+        return true;
+    }
 }
